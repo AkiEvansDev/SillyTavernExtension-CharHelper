11298a0eaf9f7fb00673e02d7bc46ca90a94c180
Aki Evans <aki.evans.dev@gmail.com>
2024-07-21 13:30:55 +0300

    add restore btn

--- src/Settings.js
+++ src/Settings.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { DEFAULT_TEMPLATE, DEFAULT_RESOURCE } from './common/data'
 import StateManager from './common/StateManager';
 
 function Settings() {
@@ -25,16 +26,21 @@ function Settings() {
         <div class="char_helper_settings">
             <div class="inline-drawer">
                 <div class="inline-drawer-toggle inline-drawer-header">
-                    <b data-i18n="ext_char_helper_title">Char Helper</b>
+                    <b>Char Helper</b>
                     <div class="inline-drawer-icon fa-solid fa-circle-chevron-down down"></div>
                 </div>
                 <div class="inline-drawer-content">
-                    <label for="char_helper_template" class="checkbox_label" data-i18n="ext_char_helper_template">Template</label>
-                    <textarea id="char_helper_template" type="text" class="text_pole textarea_compact" rows="5" placeholder="" value={template} onChange={handleChangeTemplate}></textarea>
-                    <label for="char_helper_resource" class="checkbox_label" data-i18n="ext_char_helper_resource">Resource</label>
-                    <textarea id="char_helper_resource" type="text" class="text_pole textarea_compact" rows="10" placeholder="" value={resource} onChange={handleChangeResource}></textarea>
+                    <label for="char_helper_settings_template" class="title_restorable">
+                        <span>Template</span>
+                        <div class="right_menu_button interactable" id="char_helper_settings_template_restore">
+                            <i class="fa-solid fa-clock-rotate-left fa-sm"></i>
+                        </div>
+                    </label>
+                    <textarea id="char_helper_char_helper_settings_template_template" type="text" class="text_pole textarea_compact" rows="5" placeholder="" value={template} onChange={handleChangeTemplate}></textarea>
+                    <label for="char_helper_settings_resource" class="checkbox_label">Resource</label>
+                    <textarea id="char_helper_settings_resource" type="text" class="text_pole textarea_compact" rows="10" placeholder="" value={resource} onChange={handleChangeResource}></textarea>
                     <label class="checkbox_label" for="char_helper_translate">
-                        <input type="checkbox" id="char_helper_translate" checked={translate} onChange={handleChangeTranslate} />
+                        <input type="checkbox" id="char_helper_settings_translate" checked={translate} onChange={handleChangeTranslate} />
                         <span>Translate description</span>
                     </label>
                 </div>
